163a1f5404aafb494975f8fbc084e8fbcd78f828
rcioara <roxicioara@gmail.com>
2017-09-21 00:09:27 +0300

    - change angularjs version

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 	// create the module and name it scotchApp
-	var scotchApp = angular.module('scotchApp', ['ngRoute']);
+	var scotchApp = angular.module('scotchApp', ['ngRoute','ui.bootstrap']);
 
 	// configure our routes
 	scotchApp.config(function($routeProvider) {
