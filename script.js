dc03d738885b3dfda86c920deecc95ea0749c5ee
rcioara <roxicioara@gmail.com>
2017-09-21 22:47:45 +0300

    - fix search

--- script.js
+++ script.js
@@ -30,45 +30,55 @@
 	});
 
     scotchApp.service('movieService', function($http) {
+        var self = this;
+        this.movies = [];
+
         this.get = function() {
           return $http.get('https://api.themoviedb.org/3/movie/popular?api_key=cd011ce4747999c8ae715a61176561e6&language=en-US')
             .then(function(result) {
-              return result;
+              self.setMovies(result.data.results);
+              return self.movies;
             })
         };
 
         this.search = function(searchText) {
+           self.movies.length = 0;
            var searchApiPath = 'https://api.themoviedb.org/3/search/movie?api_key=cd011ce4747999c8ae715a61176561e6&language=en-US&query='+searchText+'&page=1&include_adult=false';
            return $http.get(searchApiPath)
                     .then(function(result) {
-                      return result;
+                      self.setMovies(result.data.results);
+                      return self.movies;
                     })
         }
+
+        this.setMovies = function(movies) {
+            for (var i=0; i<movies.length; i+=3){
+                            self.movies.push(movies.slice(i,i+3));
+                          }
+        }
+
+         this.getMovies = function() {
+               return this.movies;
+         }
     });
 
-	// create the controller and inject Angular's $scope
 	scotchApp.controller('mainController', function($scope, movieService) {
-		// create a message to display in our view
-		movieService.get().then( result => {
-		     var movies = [];
-             for (var i=0; i<result.data.results.length; i+=3){
-                movies.push(result.data.results.slice(i,i+3));
-              }
 
-		    $scope.movies = movies;
+		movieService.get().then( result => {
+		    $scope.movies = result;
 		});
 
 		$scope.search = () => {
 		    console.info('search text ' + $scope.searchText)
 		    return movieService.search($scope.searchText).then( result => {
-            		    return result.data.results;
-            		});
+            });
 		}
 	});
 
 	scotchApp.controller('aboutController', function($scope) {
 		$scope.message = 'Look! I am an about page.';
 	});
+
 
 	scotchApp.controller('contactController', function($scope) {
 		$scope.message = 'Contact us! JK. This is just a demo.';
